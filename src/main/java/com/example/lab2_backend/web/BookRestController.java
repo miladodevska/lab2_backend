0a6b8c300bd7dfc567dabf56d4935178517e0bdb
Mila Dodevska <dodevskamila@gmail.com>
2022-04-24 21:27:27 +0200

    Updated

--- src/main/java/com/example/lab2_backend/web/BookRestController.java
+++ src/main/java/com/example/lab2_backend/web/BookRestController.java
@@ -1,20 +1,23 @@
 package com.example.lab2_backend.web;
 
+import com.example.lab2_backend.model.Author;
 import com.example.lab2_backend.model.Book;
 import com.example.lab2_backend.model.Dto.BookDto;
+import com.example.lab2_backend.model.Enumerations.Category;
 import com.example.lab2_backend.model.Exceptions.BookNotFoundException;
 import com.example.lab2_backend.service.AuthorService;
 import com.example.lab2_backend.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "https://.herokuapp.com/") //TODO
+@CrossOrigin(origins = "https://emtlibrary.herokuapp.com/")
 @RequestMapping("/api")
 public class BookRestController {
-
     private final BookService bookService;
     private final AuthorService authorService;
 
@@ -23,25 +26,50 @@ public class BookRestController {
         this.authorService = authorService;
     }
 
-    @GetMapping
-    public List<Book> findAll(){
+    @GetMapping("/books")
+    public List<Book> findAll() {
         return this.bookService.findAll();
     }
 
+    @GetMapping("/categories")
+    public List<Category> findAllCategories() {
+        return Arrays.stream(Category.values()).collect(Collectors.toList());
+    }
+
+    @GetMapping("/authors")
+    public List<Author> findAllAuthors() {
+        return this.authorService.findAll();
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Book> findById(@PathVariable Long id){
-        return this.bookService.findById(id).map(book -> ResponseEntity.ok().body(book))
+    public ResponseEntity<Book> getBook(@PathVariable Long id) {
+        return this.bookService.findById(id)
+                .map(book -> ResponseEntity.ok().body(book))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping("/mark/{id}")
+    public ResponseEntity<Book> markAsTaken(@PathVariable Long id){
+        Book book = this.bookService.findById(id).orElseThrow(BookNotFoundException::new);
+        return this.bookService.markAsTaken(book.getName()).map(book1 -> ResponseEntity.ok().body(book1))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping("/edit/{id}")
+    public ResponseEntity<Book> editBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
+        return this.bookService.editBook(id, bookDto.getName(), bookDto.getCategory(), bookDto.getAuthor(), bookDto.getAvailableCopies())
+                .map(book -> ResponseEntity.ok().body(book))
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Book> addBook (@RequestBody BookDto bookDto){
+    public ResponseEntity<Book> addBook(@RequestBody BookDto bookDto) {
         return this.bookService.addBook(bookDto.getName(), bookDto.getCategory(), bookDto.getAuthor(), bookDto.getAvailableCopies())
                 .map(book -> ResponseEntity.ok().body(book))
                 .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 
-    @PostMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Book> deleteById(@PathVariable Long id){
         this.bookService.deleteBook(id);
         if (this.bookService.findById(id).isPresent()){
@@ -49,21 +77,4 @@ public class BookRestController {
         }
         return ResponseEntity.ok().build();
     }
-
-    @PostMapping("/mark/{id}")
-    public ResponseEntity<Book> markAsTaken(@PathVariable Long id){
-       Book book = this.bookService.findById(id).orElseThrow(BookNotFoundException::new);
-        return this.bookService.markAsTaken(book.getName()).map(book1 -> ResponseEntity.ok().body(book1))
-                .orElseGet(() -> ResponseEntity.badRequest().build());
-    }
-
-    @PostMapping("/edit/{id}")
-    public ResponseEntity<Book> editBook(@PathVariable Long id, @RequestBody BookDto bookDto){
-        return this.bookService.editBook(id, bookDto.getName(),bookDto.getCategory(),
-                        bookDto.getAuthor(), bookDto.getAvailableCopies())
-                .map(book -> ResponseEntity.ok().body(book))
-                .orElseGet(() -> ResponseEntity.badRequest().build());
-
-    }
-
 }
